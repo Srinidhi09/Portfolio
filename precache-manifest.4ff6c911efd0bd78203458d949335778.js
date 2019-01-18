c8d601dcb8b9fcfcbcdb969550eb9b219a48fb5b
Srinidhi Karnati <srinidhikarnati09@gmail.com>
2019-01-17 18:01:20 -0600

    Updates

---
+++ precache-manifest.4ff6c911efd0bd78203458d949335778.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "18fe906f95fc7dfee3ae",
+    "url": "/portfolio/static/js/main.18fe906f.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/portfolio/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "18fe906f95fc7dfee3ae",
+    "url": "/portfolio/static/css/main.b760ee52.chunk.css"
+  },
+  {
+    "revision": "5e63199cbee35414a0f7d336aacc3446",
+    "url": "/portfolio/index.html"
+  }
+];
